80c12061404ce4a37dd24a3f69a67d3af164f6bf
Natasha <natasha.sebastian19@gmail.com>
2025-01-10 22:57:43 +0000

    update

--- molecule_names.py
+++ molecule_names.py
@@ -5,6 +5,7 @@ data = {
     "mol2": "ClC(C=C1)=CC=C1C2(CCCCC2)C3=NC4=CC(N5CCN(C(CCCC(NCCN(CCO6)CC6(CC7)CCN7C(CN(N=C8)C=C8C9=CC%10=C(N9)N=CN=C%10C%11=C(C)C(NC(N%12CC(CC(C)(C)C)(O)C%12)=O)=CC(F)=C%11)=O)=O)=O)CC5)=CC=C4C(NCCN)=N3",
     "mol3": "C(C1=C2C(=CC=C1)N([C@H]1C(=O)NC(=O)CC1)C(=O)N2C)#CCOC1CCN(C[C@H]2CC[C@H](N3C=C(NC(C4=C5N=C(N6C[C@@H]7OC[C@H]6C7)C=CN5N=C4)=O)C(C(F)F)=N3)CC2)CC1",
     "mol4": "ClC1=CC(O[C@H]2C(C)(C)[C@H](NC(C3=CC=C(C#CC4CCN(C(C[C@H](NC([C@@H]5C[C@@H](O)CN5C([C@@H](C6=CC(C)=NO6)C(C)C)=O)=O)C7=CC=C(C8=C(C)N=CS8)C=C7)=O)CC4)C=C3)=O)C2(C)C)=CC=C1C#N",
+    "mol5" : "CC(N)C(O)=O"
 }
 
 with open("molecules.json", "w") as f:
